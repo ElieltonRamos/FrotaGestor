de83a44994d781c918043ca8e95a285233315ef9
Elielton <elieltonramos14@gmail.com>
2025-10-08 15:44:44 -0300

    build(frontend): :memo: atualizar versão e configurar URL da API
    - Atualiza versão do frontend para 0.3.0. - Atualiza exibição da versão na tela de login para 0.3. - Ajusta API_URL padrão para `http://10.1.254.19:3001` e comenta localhost.

--- frontend/src/app/services/api.url.ts
+++ frontend/src/app/services/api.url.ts
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://10.1.254.19:3001';
-export const API_URL = 'http://localhost:3001';
+export const API_URL = 'http://10.1.254.19:3001';
+// export const API_URL = 'http://localhost:3001';
 
 export function mapNetworkError(
   err: any,
